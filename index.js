df2cd5a4efe811d6240afc74da336f6933e5aa92
tmtemetm <teemu.j.maki@aalto.fi>
2024-05-13 11:46:39 +0300

    Tehtävä 3.3

--- index.js
+++ index.js
@@ -31,6 +31,18 @@ app.get(personsBaseUrl, (request, response) => {
   response.json(persons)
 })
 
+app.get(`${personsBaseUrl}/:id`, (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404)
+      .end()
+  }
+})
+
 app.get('/info', (request, response) => {
   response.send(
     `<div>
