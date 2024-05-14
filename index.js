e8124db8f3119ce85a507d853513e4279109a688
tmtemetm <teemu.j.maki@aalto.fi>
2024-05-14 19:11:23 +0300

    Tehtävä 3.9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+
+app.use(cors())
 
 morgan.token('body', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
