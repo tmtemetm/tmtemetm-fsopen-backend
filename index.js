33970820d5e25c9f09aab2ae279ef1918c1c3a96
tmtemetm <teemu.j.maki@aalto.fi>
2024-05-18 19:53:40 +0300

    Tehtävä 3.15

--- index.js
+++ index.js
@@ -85,11 +85,10 @@ app.post(personsBaseUrl, (request, response) => {
 })
 
 app.delete(`${personsBaseUrl}/:id`, (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204)
-    .end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
 })
 
 app.get('/info', (request, response) => {
